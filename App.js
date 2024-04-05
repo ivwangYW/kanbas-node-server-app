a9836f4e931103a35530d801df2356f26c066dfd
Yiwei Wang <ivwang@yahoo.com>
2024-04-04 23:10:32 -0400

    updated

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 //const express = require('express')                // equivalent to import
 import express  from 'express';
-import Hello from './hello.js';
+import Hello from './Hello.js';
 import Lab5 from './Lab5.js';
 const app = express();  // create new express instance
 app.use(express.json());
@@ -8,4 +8,4 @@ app.use(express.json());
 Lab5(app);
 Hello(app);
 
-app.listen(process.env.PORT || 4000);// listen to the environment variable called PORT for deployment to remote server, or use the local port http://localhost:4000 when running locally. 
+app.listen(4000);// listen to the environment variable called PORT for deployment to remote server, or use the local port http://localhost:4000 when running locally. 
