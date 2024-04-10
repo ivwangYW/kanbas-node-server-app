671edcd41005f1a5cdb00b7fc97b0855fe7c9e20
Yiwei Wang <ivwang@yahoo.com>
2024-04-10 18:32:49 -0400

    updated

--- Kanbas/modules/routes.js
+++ Kanbas/modules/routes.js
@@ -14,5 +14,34 @@ function ModuleRoutes(app) {
         
     }
     );
+
+
+    app.post("/api/courses/:cid/modules", (req,res)=>{
+        console.log("app.post function on server is processing.");
+        const {cid} = req.params;
+        const newModule = {
+            ...req.body, 
+            course: cid,
+            _id:new Date().getTime().toString()
+        };
+        Database.modules.push(newModule);
+        res.send(newModule);
+    });
+
+    app.delete("/api/modules/:mid", (req, res)=>{
+        const {mid} = req.params;
+            Database.modules = Database.modules.filter((m)=>m._id!== mid);
+            res.sendStatus(200);
+        
+    }
+    );
+
+    app.post("/api/modules/:mid", (req, res)=>{
+        const {mid} = req.params;
+        const moduleIndex = Database.modules.findIndex((m)=>m._id ===mid);
+        Database.modules[moduleIndex] = {...Database.modules[moduleIndex], ...req.body};
+        res.sendStatus(204);
+    });
+
 }
 export default ModuleRoutes; 
