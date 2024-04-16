85c4dbb7037471484f5c0d5e5a31212e0a5225b6
Yiwei Wang <ivwang@yahoo.com>
2024-04-16 14:40:24 -0400

    updated

--- App.js
+++ App.js
@@ -6,6 +6,10 @@ import cors from "cors";
 import CourseRoutes from './Kanbas/courses/routes.js';
 import ModuleRoutes from './Kanbas/modules/routes.js';
 import AssignmentRoutes from './Kanbas/assignments/routes.js';
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
+//
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();  // create new express instance
 app.use(cors());   //app.use(express.json());
 app.use(express.json());   //Configure JSON HTTP body parsing FIRST. Make sure that this appears after the app.use(cors());                          
@@ -14,6 +18,7 @@ Hello(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 
 
 app.listen(process.env.PORT || 4000);// listen to the environment variable called PORT for deployment to remote server, or use the local port http://localhost:4000 when running locally. 
